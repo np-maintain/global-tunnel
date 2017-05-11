93424696bd8bcd12450458ac3dfbcdccf7c527d6
Eugene Mirotin <emirotin@gmail.com>
2017-05-11 13:44:58 +0300

    fix URL serialization

--- index.js
+++ index.js
@@ -62,7 +62,7 @@ function tryParse(url) {
 function stringifyProxy(conf) {
   return urlStringify({
     protocol: conf.protocol,
-    host: conf.host,
+    hostname: conf.host,
     port: conf.port,
     auth: conf.proxyAuth
   })
